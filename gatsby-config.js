b6c4c1be5a5c165b9dc4a67a9a41264e00596a13
gtiamzon <gerald.tiamzon@yahoo.com>
2021-10-04 00:53:56 -0700

    phone background color changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#646464`,
+        theme_color: `#646464`,
         display: `minimal-ui`,
         icon: `src/images/GeraldsLogoFINAL.png`, // This path is relative to the root of the site.
       },
